58940228448de0a01ec9dbcda3414cf98aa4e7b4
JBL-987 <jasonbrandonloi03@gmail.com>
2025-01-01 20:20:52 +0700

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone', // Use 'standalone' mode for easier deployment
-  }),
+   adapter: netlify(), 
 });
