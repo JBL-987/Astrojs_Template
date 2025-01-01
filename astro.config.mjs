522ac43198461244bd99080437059ff250c646a0
JBL-987 <jasonbrandonloi03@gmail.com>
2025-01-01 14:41:08 +0700

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node'; // Ganti dengan adapter yang sesuai
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  output: 'static',
   adapter: node({ mode: 'standalone' }), // Konfigurasi adapter
 });
