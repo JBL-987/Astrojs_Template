4ea4cdd90bc7211f0bbb24ab5836bf00216c2b2c
JBL-987 <jasonbrandonloi03@gmail.com>
2025-01-01 15:45:28 +0700

    commit

--- dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DjQG7QBu.mjs';
-export { renderers } from '../renderers.mjs';
--- dist/server/pages/home.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../chunks/Home_B5eafyxs.mjs';
-export { renderers } from '../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
